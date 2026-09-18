3e0bbd03f81577356c63a65ada4834a3dd174edd
João Rodrigues <joaovitor550dito@gmail.com>
2026-09-18 18:32:20 -0300

    Adiciona ClasseA e ClasseB

---
+++ classeA.h
@@ -0,0 +1,42 @@
+#ifndef CLASSEA_H
+#define CLASSEA_H
+
+#include <iostream>
+
+class ClasseA
+{
+private:
+    int A1;
+    float A2;
+
+public:
+    int getA1()
+    {
+        return A1;
+    }
+    void setA1(int a1)
+    {
+        A1 = a1;
+    }
+
+    float getA2()
+    {
+        return A2;
+    }
+    void setA2(float a2)
+    {
+        A2 = a2;
+    }
+
+    void MA1()
+    {
+        std::cout << "MA1" << std::endl;
+    }
+
+    void MA2()
+    {
+        std::cout << "MA2" << std::endl;
+    }
+};
+
+#endif
---
+++ classeB.h
@@ -0,0 +1,42 @@
+#ifndef CLASSEB_H
+#define CLASSEB_H
+
+#include <iostream>
+
+class ClasseB
+{
+private:
+    int B1;
+    float B2;
+
+public:
+    int getB1()
+    {
+        return B1;
+    }
+    void setB1(int b1)
+    {
+        B1 = b1;
+    }
+
+    float getB2()
+    {
+        return B2;
+    }
+    void setB2(float b2)
+    {
+        B2 = b2;
+    }
+
+    void MB1()
+    {
+        std::cout << "MB1" << std::endl;
+    }
+
+    void MB2()
+    {
+        std::cout << "MB2" << std::endl;
+    }
+};
+
+#endif
