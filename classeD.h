eb9855ec6e7436f878153860d953b701122e1486
João Rodrigues <joaovitor550dito@gmail.com>
2026-09-19 17:06:32 -0300

    classeD criada

---
+++ classeD.h
@@ -0,0 +1,24 @@
+#ifndef CLASSED_H
+#define CLASSED_H
+
+#include <iostream>
+
+class ClasseD
+{
+private:
+    int D1;
+    int D2;
+
+public:
+    void MD1()
+    {
+        std::cout << "MD1" << std::endl;
+    }
+
+    void MD2()
+    {
+        std::cout << "MD2" << std::endl;
+    }
+};
+
+#endif
